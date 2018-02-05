16a884c61859f7571d4152d1622789131e431117
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2018-02-05 16:56:16 +0000

    select_evaluate_scatter function

--- code/fly-vs-fly/util.py
+++ code/fly-vs-fly/util.py
@@ -40,3 +40,27 @@ def get_enc(position, angle, fov_angle, num_bins, other_positions):
             ), 0, num_bins - 1))
             result[i] = 1
     return result
+
+
+def select_evaluate_scatter(inputs, indices, functions, out_dim=None):
+    """Pass selected inputs through selected functions.
+
+    input:
+        inputs: input to the functions; Variable [batch_size, in_dim]
+        indices: Variable [batch_size] with each elememt in {0, ..., num_functions - 1}
+        functions: list of functions of length num_functions; each function takes in [-1, in_dim] and outputs [-1, out_dim]
+        out_dim: output dimension of each function
+
+    output: Variable [batch_size, out_dim] where output[b, :] = functions[indices[b]](input[b, :])
+    """
+
+    if out_dim is None:
+        out_dim = functions[0](inputs[0].unsqueeze(0)).size(1)
+
+    num_functions = len(functions)
+    batch_size, in_dim = inputs.size()
+    result = Variable(inputs.data.new(batch_size, out_dim))
+    for f_idx, f in enumerate(functions):
+        indices_for_f_idx = (indices == f_idx).nonzero().squeeze(1)
+        result[indices_for_f_idx, :] = f(inputs[indices_for_f_idx, :])
+    return result
