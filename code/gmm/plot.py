6bcc470867fb626f2a2db9666b709f5a04a18044
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2018-03-20 14:33:45 +0000

    plot gradient means

--- code/gmm/plot.py
+++ code/gmm/plot.py
@@ -26,7 +26,7 @@ def main(args):
     logging_iterations = np.arange(0, num_iterations, logging_interval)
 
     # Plotting
-    fig, axs = plt.subplots(6, 1, sharex=True)
+    fig, axs = plt.subplots(7, 1, sharex=True)
     fig.set_size_inches(3.25, 8)
 
     for ax in axs:
@@ -34,7 +34,7 @@ def main(args):
         ax.spines['right'].set_visible(False)
 
     ## IWAE
-    iwae_filenames = ['log_evidence_history', 'elbo_history', 'posterior_norm_history', 'true_posterior_norm_history', 'p_mixture_probs_norm_history', 'mean_multiplier_history', 'p_grad_std_history', 'q_grad_std_history']
+    iwae_filenames = ['log_evidence_history', 'elbo_history', 'posterior_norm_history', 'true_posterior_norm_history', 'p_mixture_probs_norm_history', 'mean_multiplier_history', 'p_grad_std_history', 'q_grad_std_history', 'q_grad_mean_history']
 
     if args.all or args.reinforce:
         iwae_reinforce = dict(zip(
@@ -51,6 +51,7 @@ def main(args):
         axs[3].plot(logging_iterations, iwae_reinforce['p_mixture_probs_norm_history'], linestyle=':', color='0.5', label='reinforce')
         axs[4].plot(logging_iterations, iwae_reinforce['posterior_norm_history'], linestyle=':', color='0.5', label='reinforce')
         axs[5].plot(logging_iterations, iwae_reinforce['true_posterior_norm_history'], linestyle=':', color='0.5', label='reinforce')
+        axs[6].plot(logging_iterations, iwae_reinforce['q_grad_mean_history'], linestyle=':', color='0.5', label='reinforce')
 
     if args.all or args.vimco:
         iwae_vimco = dict(zip(
@@ -67,9 +68,10 @@ def main(args):
         axs[3].plot(logging_iterations, iwae_vimco['p_mixture_probs_norm_history'], linestyle=':', color='black', label='vimco')
         axs[4].plot(logging_iterations, iwae_vimco['posterior_norm_history'], linestyle=':', color='black', label='reinforce')
         axs[5].plot(logging_iterations, iwae_vimco['true_posterior_norm_history'], linestyle=':', color='black', label='reinforce')
+        axs[6].plot(logging_iterations, iwae_vimco['q_grad_mean_history'], linestyle=':', color='black', label='reinforce')
 
     ## RWS
-    rws_filenames = ['log_evidence_history', 'posterior_norm_history', 'true_posterior_norm_history', 'p_mixture_probs_norm_history', 'mean_multiplier_history', 'p_grad_std_history', 'q_grad_std_history']
+    rws_filenames = ['log_evidence_history', 'posterior_norm_history', 'true_posterior_norm_history', 'p_mixture_probs_norm_history', 'mean_multiplier_history', 'p_grad_std_history', 'q_grad_std_history', 'q_grad_mean_history']
 
     if args.all or args.ws:
         ws = dict(zip(
@@ -86,6 +88,7 @@ def main(args):
         axs[3].plot(logging_iterations, ws['p_mixture_probs_norm_history'], linestyle='-.',color='0.5', label='ws')
         axs[4].plot(logging_iterations, ws['posterior_norm_history'], linestyle='-.',color='0.5', label='ws')
         axs[5].plot(logging_iterations, ws['true_posterior_norm_history'], linestyle='-.',color='0.5', label='ws')
+        axs[6].plot(logging_iterations, ws['q_grad_mean_history'], linestyle='-.',color='0.5', label='ws')
 
     if args.all or args.ww:
         ww = dict(zip(
@@ -102,6 +105,7 @@ def main(args):
         axs[3].plot(logging_iterations, ww['p_mixture_probs_norm_history'], linestyle='--',color='0.8', label='ww')
         axs[4].plot(logging_iterations, ww['posterior_norm_history'], linestyle='--',color='0.8', label='ww')
         axs[5].plot(logging_iterations, ww['true_posterior_norm_history'], linestyle='--',color='0.8', label='ww')
+        axs[6].plot(logging_iterations, ww['q_grad_mean_history'], linestyle='--',color='0.8', label='ww')
 
     if args.all or args.wsw:
         wsw = dict(zip(
@@ -118,6 +122,7 @@ def main(args):
         axs[3].plot(logging_iterations, wsw['p_mixture_probs_norm_history'], linestyle='--',color='0.5', label='wsw')
         axs[4].plot(logging_iterations, wsw['posterior_norm_history'], linestyle='--',color='0.5', label='wsw')
         axs[5].plot(logging_iterations, wsw['true_posterior_norm_history'], linestyle='--',color='0.5', label='wsw')
+        axs[6].plot(logging_iterations, wsw['q_grad_mean_history'], linestyle='--',color='0.5', label='wsw')
 
     if args.all or args.wswa:
         wswa = dict(zip(
@@ -134,6 +139,7 @@ def main(args):
         axs[3].plot(logging_iterations, wswa['p_mixture_probs_norm_history'], linestyle='-',color='0.5', label='wswa')
         axs[4].plot(logging_iterations, wswa['posterior_norm_history'], linestyle='-',color='0.5', label='wswa')
         axs[5].plot(logging_iterations, wswa['true_posterior_norm_history'], linestyle='-',color='0.5', label='wswa')
+        axs[6].plot(logging_iterations, wswa['q_grad_mean_history'], linestyle='-',color='0.5', label='wswa')
 
     axs[0].axhline(true_log_evidence, linestyle='-', color='black', label='true')
     axs[0].set_ylabel('Avg. test\nlog evidence')
@@ -150,8 +156,8 @@ def main(args):
     axs[3].set_ylabel('L2 of mixture probs.\n to true mixture probs.')
 
     axs[4].set_ylabel('Avg. test L2 of\nq to current p')
-
     axs[5].set_ylabel('Avg. test L2 of\nq to true p')
+    axs[6].set_ylabel('Avg. mean of $\phi$ \n gradient est.')
 
     axs[-1].legend(ncol=4, loc='upper center', bbox_to_anchor=(0.5, -0.4))
     axs[-1].set_xlabel('Iteration')
