2d2aea90b52a3932e03ba9d7d23eaac9b5bbc295
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2018-04-21 23:17:52 +0100

    move helper functions to util.py

--- code/gmm/gmm.py
+++ code/gmm/gmm.py
@@ -19,56 +19,6 @@ rws_phi_optim_params = {'lr': 1e-3}
 softmax_multiplier = 0.5
 
 
-def lognormexp(values, dim=0):
-    """Exponentiates, normalizes and takes log of a Tensor/Variable/np.ndarray.
-
-    input:
-        values: Tensor/Variable/np.ndarray [dim_1, ..., dim_N]
-        dim: n
-    output:
-        result: Tensor/Variable/np.ndarray [dim_1, ..., dim_N]
-            where result[i_1, ..., i_N] =
-
-                                 exp(values[i_1, ..., i_N])
-            log( ------------------------------------------------------------ )
-                    sum_{j = 1}^{dim_n} exp(values[i_1, ..., j, ..., i_N])
-    """
-
-    if isinstance(values, np.ndarray):
-        log_denominator = scipy.special.logsumexp(
-            values, axis=dim, keepdims=True
-        )
-        # log_numerator = values
-        return values - log_denominator
-    else:
-        log_denominator = logsumexp(values, dim=dim, keepdim=True)
-        # log_numerator = values
-        return values - log_denominator
-
-
-def logsumexp(values, dim=0, keepdim=False):
-    """Logsumexp of a Tensor/Variable.
-
-    See https://en.wikipedia.org/wiki/LogSumExp.
-
-    input:
-        values: Tensor/Variable [dim_1, ..., dim_N]
-        dim: n
-
-    output: result Tensor/Variable
-        [dim_1, ..., dim_{n - 1}, dim_{n + 1}, ..., dim_N] where
-
-        result[i_1, ..., i_{n - 1}, i_{n + 1}, ..., i_N] =
-            log(sum_{i_n = 1}^N exp(values[i_1, ..., i_N]))
-    """
-
-    values_max, _ = torch.max(values, dim=dim, keepdim=True)
-    result = values_max + torch.log(torch.sum(
-        torch.exp(values - values_max), dim=dim, keepdim=True
-    ))
-    return result if keepdim else result.squeeze(dim)
-
-
 class MixtureDistribution:
     def __init__(self, sample_lambdas, logpdf_lambdas, mixture_probs):
         self.sample_lambdas = sample_lambdas
