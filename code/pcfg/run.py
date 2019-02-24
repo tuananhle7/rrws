fa1f5abd2035ea657bf93dd49a23ce5c061ef77e
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2019-02-24 15:40:00 +0000

    refactor run.py

--- code/pcfg/run.py
+++ code/pcfg/run.py
@@ -1,76 +1,84 @@
 import util
 import train
+import os
 
 
-def main(args):
+def get_models(load_model_folder, pcfg_path):
+    """Load/initialize models.
+
+    Args:
+        load_model_folder: string; if '' then will initialize new models
+        pcfg_path: path to a json spec of a pcfg
+
+    Returns: generative_model, inference_network, true_generative_model
+    """
+
+    if load_model_folder == '':
+        generative_model, inference_network, true_generative_model = \
+            util.init_models(args.pcfg_path)
+    else:
+        _, _, true_generative_model = util.init_models(args.pcfg_path)
+        generative_model, inference_network = util.load_models()
+
+    return generative_model, inference_network, true_generative_model
+
+
+def run(args):
     print(args)
 
     # init models
-    if args.load_model:
-        _, _, true_generative_model = util.init_models(args.pcfg_path)
-        generative_model, inference_network = util.load_models()
-    else:
-        generative_model, inference_network, true_generative_model = \
-            util.init_models(args.pcfg_path)
+    generative_model, inference_network, true_generative_model = get_models(
+        args.load_model_folder, args.pcfg_path)
 
     # train
-    if args.train_mode == 'sleep':
-        train_sleep_callback = train.TrainSleepCallback(args.logging_interval)
-        train.train_sleep(true_generative_model, inference_network,
-                          args.num_samples, args.num_iterations,
-                          train_sleep_callback)
-        model_folder = './trained_models/sleep'
-    elif args.train_mode == 'ww':
-        model_folder = './trained_models/ww'
-        train_wake_wake_callback = train.TrainWakeWakeCallback(
+    model_folder = os.path.join('./trained_models/', args.train_mode)
+    stats_filename = util.get_stats_filename(model_folder)
+    if args.train_mode == 'ww':
+        train_callback = train.TrainWakeWakeCallback(
             args.pcfg_path, model_folder, true_generative_model,
             args.logging_interval, args.checkpoint_interval,
             args.eval_interval)
         train.train_wake_wake(generative_model, inference_network,
                               true_generative_model, args.batch_size,
                               args.num_iterations, args.num_particles,
-                              train_wake_wake_callback)
+                              train_callback)
         stats_filename = util.get_stats_filename(model_folder)
-        util.save_object(train_wake_wake_callback, stats_filename)
+        util.save_object(train_callback, stats_filename)
     elif args.train_mode == 'ws':
-        model_folder = './trained_models/ws'
-        train_wake_sleep_callback = train.TrainWakeSleepCallback(
+        train_callback = train.TrainWakeSleepCallback(
             args.pcfg_path, model_folder, true_generative_model,
             args.logging_interval, args.checkpoint_interval,
             args.eval_interval)
         train.train_wake_sleep(generative_model, inference_network,
                                true_generative_model, args.batch_size,
                                args.num_iterations, args.num_particles,
-                               train_wake_sleep_callback)
+                               train_callback)
         stats_filename = util.get_stats_filename(model_folder)
-        util.save_object(train_wake_sleep_callback, stats_filename)
+        util.save_object(train_callback, stats_filename)
     elif args.train_mode == 'reinforce' or args.train_mode == 'vimco':
-        algorithm = args.train_mode
-        model_folder = './trained_models/{}'.format(algorithm)
-        train_iwae_callback = train.TrainIwaeCallback(
+        train_callback = train.TrainIwaeCallback(
             args.pcfg_path, model_folder, true_generative_model,
             args.logging_interval, args.checkpoint_interval,
             args.eval_interval)
-        train.train_iwae(algorithm, generative_model, inference_network,
+        train.train_iwae(args.train_mode, generative_model, inference_network,
                          true_generative_model, args.batch_size,
                          args.num_iterations, args.num_particles,
-                         train_iwae_callback)
-        stats_filename = util.get_stats_filename(model_folder)
-        util.save_object(train_iwae_callback, stats_filename)
+                         train_callback)
 
-    # save models
+    # save models and stats
     util.save_models(generative_model, inference_network, args.pcfg_path,
                      model_folder)
+    util.save_object(train_callback, stats_filename)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--load-model', dest='load_model', action='store_true',
-                        help=' ')
+    parser.add_argument('--load-model-folder', default='',
+                        help='if specified, will train loaded model')
     parser.add_argument('--train-mode', default='ww',
-                        help=' ')
+                        help='ww, ws, reinforce or vimco')
     parser.add_argument('--num-iterations', type=int, default=2000,
                         help=' ')
     parser.add_argument('--logging-interval', type=int, default=10,
@@ -85,7 +93,5 @@ if __name__ == '__main__':
                         help=' ')
     parser.add_argument('--pcfg-path', default='./pcfgs/astronomers_pcfg.json',
                         help=' ')
-    parser.add_argument('--num-samples', type=int, default=100,
-                        help='only needed for pure sleep')
     args = parser.parse_args()
-    main(args)
+    run(args)
