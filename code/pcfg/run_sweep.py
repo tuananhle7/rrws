2e31f6649f2b2c12a606bec27e4288c5d22f65b1
Tuan Anh Le <tuananhl@cs.ubc.ca>
2019-02-25 08:16:19 -0800

    fix run scripts and docker

--- code/pcfg/run_sweep.py
+++ code/pcfg/run_sweep.py
@@ -2,16 +2,16 @@ import subprocess
 
 
 def main():
-    num_iterations = 10
+    num_iterations = 100
     logging_interval = 1
     eval_interval = 1
     checkpoint_interval = 5
     batch_size = 2
     for seed in [1, 2, 3, 4, 5]:
         for train_mode in ['ws', 'vimco', 'ww']:
-            for num_particles in [2, 3, 4]:
+            for num_particles in [2, 3, 4, 5, 6]:
                 subprocess.call(
-                    'qsub -v ws {} {} {} {} {} {} {} run.sh'.format(
+                    'qsub -F \"{} {} {} {} {} {} {}\" run.sh'.format(
                         train_mode, num_iterations, logging_interval,
                         eval_interval, checkpoint_interval, batch_size,
                         num_particles), shell=True)
