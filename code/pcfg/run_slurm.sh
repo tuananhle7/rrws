f35639e1a886d8da31616e11e513c881c6f595cd
Tuan Anh Le <magd3733@oscgate.arcus.osc.local>
2019-02-28 16:06:11 +0000

    fix slurm job script

--- code/pcfg/run_slurm.sh
+++ code/pcfg/run_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=200:00:00
+#SBATCH --time=100:00:00
 #SBATCH --job-name=rws-pcfg
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tuananh@robots.ox.ac.uk
@@ -19,21 +19,15 @@ BATCH_SIZE=$6
 NUM_PARTICLES=$7
 SEED=$8
 
-echo `date +%Y-%m-%d_%H:%M:%S` build docker image
-docker build -t rws-pcfg .
-
-echo `date +%Y-%m-%d_%H:%M:%S` start docker container
-id=$(docker run -it -d --rm -v $PWD:/workspace rws-pcfg)
-
-echo `date +%Y-%m-%d_%H:%M:%S` run run.py in docker container
-docker exec $id python -u run.py --train-mode $TRAIN_MODE \
-                                 --num-iterations $NUM_ITERATIONS \
-                                 --logging-interval $LOGGING_INTERVAL \
-                                 --eval-interval $EVAL_INTERVAL \
-                                 --checkpoint-interval $CHECKPOINT_INTERVAL \
-                                 --batch-size $BATCH_SIZE \
-                                 --num-particles $NUM_PARTICLES \
-                                 --seed $SEED 2>&1 | tee ./jobs_out_err/rws_pcfg_%j_temp.out_err
-
-echo `date +%Y-%m-%d_%H:%M:%S` stop docker container
-docker stop $id
+module load python/anaconda3/5.0.1
+source activate $HOME/torch-env
+python --version
+python -c "import torch; print('torch version = {}'.format(torch.__version__))"
+python -u run.py --train-mode $TRAIN_MODE \
+                 --num-iterations $NUM_ITERATIONS \
+                 --logging-interval $LOGGING_INTERVAL \
+                 --eval-interval $EVAL_INTERVAL \
+                 --checkpoint-interval $CHECKPOINT_INTERVAL \
+                 --batch-size $BATCH_SIZE \
+                 --num-particles $NUM_PARTICLES \
+                 --seed $SEED 2>&1 | tee ./jobs_out_err/rws_pcfg_${SLURM_JOB_ID}_temp.out_err
