372753c552b1e29db008cb877a12a268407bd36e
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2019-03-01 17:49:01 +0000

    typo

--- code/pcfg/util.py
+++ code/pcfg/util.py
@@ -559,7 +559,7 @@ def rgba2gray(rgba):
     return np.dot(rgba[..., :3], [0.299, 0.587, 0.114])
 
 
-# This functions needs to be optimized
+# This function needs to be optimized
 def xsys2gray(xs, ys):
     """Args:
         xs: tensor of shape [100]
